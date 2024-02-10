0a0c3da6de40ad537c315927546aa8f0267757b0
kiruiaaron <kiruiaaron471@gmail.com>
2024-02-10 18:52:03 +0300

    updated the latest changes

--- main.go
+++ main.go
@@ -1,125 +1,238 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-//structures in go
+func increment(x int) int {
+	x++
+	return x
+}
+
+func getNames() (string, string) {
+	return "John", "Doe"
+}
+
+// structures in go
 type sender struct {
 	rateLimit int
 	user
 }
 
 type user struct {
-	name string 
+	name   string
 	number int
 }
 
-
-//function in go
-func test(s sender){
+// function in go
+func test(s sender) {
 	fmt.Println("Sender name:", s.name)
 	fmt.Println("Sender number:", s.number)
 	fmt.Println("Sender rateLimit:", s.rateLimit)
 	fmt.Println("=========================")
 }
 
-func sendSMSToCouple(msgToCustomer, msgToSpouse string) (float64, error){
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (float64, error) {
 
 	costToCustomer, err := sendSMS(msgToCustomer)
-     //Handling errors in go
+	//Handling errors in go
 	if err != nil {
 		return 0.0, err
 	}
 
 	costForSpouse, err := sendSMS(msgToSpouse)
-   //handling errors in Go
-	if err != nil{
+	//handling errors in Go
+	if err != nil {
 		return 0.0, err
 	}
 
-	return costToCustomer + costForSpouse , nil
+	return costToCustomer + costForSpouse, nil
 }
 
-//FIZZBUZZ
-func fizzBuzz(){
-   for i := 1; i<= 100; i++{
-	if i%3 == 0 && i%5 == 0{
-		fmt.Println("Fizzbuzz")
-	}else if i%3 == 0{
-       fmt.Println("fizz")
-	}else if i%5 == 0{
-		fmt.Println("buzz")
-	}else{
-		fmt.Println(i)
+// FIZZBUZZ
+func fizzBuzz() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("Fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
 	}
-   }
 }
-
 
 //Print Primes. It should print all of the prime numbers up to and including max.
 //It should skip any numbers that are not prime
 
-
-func printPrimes(max int){
-     for n :=2; n < max+1; n++{
-		if n == 2{
+func printPrimes(max int) {
+	for n := 2; n < max+1; n++ {
+		if n == 2 {
 			fmt.Println(n)
 			continue
 		}
-		if n%2 == 0{
+		if n%2 == 0 {
 			continue
 		}
 		isPrime := true
-		for i := 3;i * i < n+1; i++{
-			if n % i == 0{	
+		for i := 3; i*i < n+1; i++ {
+			if n%i == 0 {
 				isPrime = false
 				break
 			}
-		}	
-		if !isPrime{
+		}
+		if !isPrime {
 			continue
 		}
 		fmt.Println(n)
-	 }
+	}
 }
 
-
-
-func prime(max int){
+func prime(max int) {
 	fmt.Printf("Primes up to %v:\n", max)
 	printPrimes(max)
 	fmt.Println("================================")
 }
 
-
-func sendSMS( message string) (float64, error){
+func sendSMS(message string) (float64, error) {
 	const maxTextLen = 25
 	const costPerChar = 0.0002
 
-	if len(message)  > maxTextLen {
+	if len(message) > maxTextLen {
 		return 0.0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
 	}
 	return costPerChar * float64(len(message)), nil
 }
 
-
-
-func send(msgToCustomer, msgToSpouse string){
+func send(msgToCustomer, msgToSpouse string) {
 	defer fmt.Println("============")
 	fmt.Println("Message for customer:", msgToCustomer)
 	fmt.Println("Message for spouse:", msgToSpouse)
 
 	totalCost, err := sendSMSToCouple(msgToCustomer, msgToSpouse)
 	//handling errors in go
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error:", err)
-		return 
+		return
 	}
 
 	fmt.Printf("Total cost: $%.4f", totalCost)
 }
 
+//Arrays in go
+//Arrays are fixed ordered list
 
-//The point of entry for execution in Go
+func getMessageWithRetries() [3]string {
+	return [3]string{
+		"click here to sign up",
+		"pretty please click here",
+		"we beg you to sign up",
+	}
+}
+
+func sendFromArray(name string, doneAt int) {
+	fmt.Printf("sending to %v...", name)
+	fmt.Println()
+
+	messages := getMessageWithRetries()
+	for i := 0; i < len(messages); i++ {
+		msg := messages[i]
+		fmt.Printf(`sending: "%v"`, msg)
+		fmt.Println()
+		if i == doneAt {
+			fmt.Println("they responded")
+			break
+		}
+		if i == len(messages)-1 {
+			fmt.Println("complete failure")
+		}
+	}
+}
+
+// Slices in go
+const (
+	planFree = "free"
+	planPro  = "pro"
+)
+
+func getMessageWithRetriesPlan(plan string) ([]string, error) {
+	allMessages := getMessageWithRetries()
+	if plan == planPro {
+		return allMessages[:], nil
+	}
+	if plan == planFree {
+		return allMessages[0:2], nil
+	}
+	return nil, errors.New("unsupported plan")
+}
+
+// Variadic function and spread operator
+func printStrings(strings ...string) {
+	for i := 0; i < len(strings); i++ {
+		fmt.Println(strings[i])
+	}
+}
+
+// APPEND
+// The built-in append function is used to dynamically add elements to a slice:
+// Syntax: func append(slice []Type, elems ...Type) []Type
+type cost struct {
+	day   int
+	value float64
+}
+
+func getCostsByDay(costs []cost) []float64 {
+	costsByDay := []float64{}
+	for i := 0; i < len(costs); i++ {
+		cost := costs[i]
+		for cost.day >= len(costsByDay) {
+			costsByDay = append(costsByDay, 0.0)
+		}
+		costsByDay[cost.day] += cost.value
+	}
+	return costsByDay
+}
+
+func appendCost(costs []cost){
+	fmt.Printf("Creating daily buckets for %v costs...\n", len(costs))
+	costsByday := getCostsByDay(costs)
+	fmt.Println("costs by day:")
+	for i := 0; i < len(costsByday); i++{
+		fmt.Printf("- Day %v: %.2f\n",i, costsByday[i])
+	}
+	fmt.Println("===== END REPORT ======")
+}
+
+
+//SLICE OF SLICES
+
+func createMatrix(rows, cols int) [][]int{
+	matrix := make([][]int,0)
+	for i := 0; i < rows; i++{
+		row := make([]int, 0)
+		for j := 0; j < cols; j++{
+           row = append(row, i*j)
+		}
+		matrix = append(matrix, row)
+	}
+	return matrix
+}
+
+func testMatrix(rows, cols int) {
+	fmt.Printf("Creating %v x %v matrix...\n", rows, cols)
+	matrix := createMatrix(rows, cols)
+	for i := 0; i < len(matrix); i++ {
+		fmt.Println(matrix[i])
+	}
+	fmt.Println("===== END REPORT =====")
+}
+
+
+
+// The point of entry for execution in Go
 func main() {
 
 	x := 5
@@ -132,64 +245,71 @@ func main() {
 
 	fmt.Println("Welcome to Textio ,", firstName)
 
-
-
 	test(sender{
 		rateLimit: 10000,
-		user:user{
-			name: "Deborah",
-			number:18055558790,
+		user: user{
+			name:   "Deborah",
+			number: 18055558790,
 		},
 	})
 	test(sender{rateLimit: 5000,
-	user: user{
-		name: "Sarah",
-		number: 19055558790,
-	},
-})
+		user: user{
+			name:   "Sarah",
+			number: 19055558790,
+		},
+	})
 
-test(sender{rateLimit: 1000,
-	user: user{
-		name: "Sally",
-		number: 19055558790,
-	},
-})
+	test(sender{rateLimit: 1000,
+		user: user{
+			name:   "Sally",
+			number: 19055558790,
+		},
+	})
+
+	send(
+		"Thanks for coming in to our flower shop today!",
+		"We hope you enjoyed your gift.",
+	)
+	send(
+		"Thanks for joining us!",
+		"Have a good day.",
+	)
+	send(
+		"Thank you.",
+		"Enjoy!",
+	)
+	send(
+		"We loved having you in!",
+		"We hope the rest of your evening is absolutely fantastic.",
+	)
+
+	//fizzbuzz
+	fizzBuzz()
+
+	//PrintPrime
+	prime(10)
+	prime(20)
+	prime(30)
+
+	sendFromArray("Bob", 3)
+	sendFromArray("Alice", 1)
+	sendFromArray("Mangalam", 2)
+	sendFromArray("Ozgur", 3)
+
+	names := []string{"bob", "sue", "alice"}
+	printStrings(names...) //spread operator
 
 
+	appendCost([]cost{
+		{0, 1.0},
+		{1,2.0},
+		{1,3.1},
+		{2,2.5},
+	})
 
-send(
-	"Thanks for coming in to our flower shop today!",
-	"We hope you enjoyed your gift.",
-)
-send(
-	"Thanks for joining us!",
-	"Have a good day.",
-)
-send(
-	"Thank you.",
-	"Enjoy!",
-)
-send(
-	"We loved having you in!",
-	"We hope the rest of your evening is absolutely fantastic.",
-)
+	testMatrix(3,3)
+	testMatrix(5,5)
+	testMatrix(10,10)
+	testMatrix(15,15)
 
-
-//fizzbuzz
-fizzBuzz()
-
-//PrintPrime
-prime(10)
-prime(20)
-prime(30)
-
-}
-
-func increment(x int) int {	
-	x++
-	return x
-}
-
-func getNames() (string, string) {
-	return "John", "Doe"
 }
